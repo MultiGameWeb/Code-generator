ae19e00483537681c20206ed15f9e26a9b2d1391
MultiGameWeb <kammarivinaykumar216@gmail.com>
2026-09-21 00:07:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import rateLimit from "express-rate-limit";
+import { rateLimit } from "express-rate-limit";
 import crypto from "crypto";
 import { GoogleGenAI } from "@google/genai";
 
